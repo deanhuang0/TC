d9a60c452c43709afb8567774a091a672740fe96
deanhuang0 <deanhuang0@163.com>
2015-02-27 23:05:12 +0800

    sdfasdf

---
+++ app/src/androidTest/java/com/example/huangbin/myapplication/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.example.huangbin.myapplication;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    private int test = 0;
+    public ApplicationTest() {
+        super(Application.class);
+    }ddd
+}
